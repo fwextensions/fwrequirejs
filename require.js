04d46e3d72dc3795d499cdfe39e9e9e40f6cff87
John Dunning <fw@johndunning.com>
2012-06-24 14:58:48 -0700

    Trying to force require.js to use spaces.

--- require.js
+++ require.js
@@ -1,6 +1,6 @@
 /** vim: et:ts=4:sw=4:sts=4
  * @license RequireJS 2.0.0 Copyright (c) 2010-2012, The Dojo Foundation All Rights Reserved.
- * Available via the MIT or new BSD license.
+ * Available via the MIT or new BSD license. 
  * see: http://github.com/jrburke/requirejs for details
  */
 /*jslint regexp: true, nomen: true */
